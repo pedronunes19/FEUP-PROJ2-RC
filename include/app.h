a10b2fe827479de042190c049be277190473316b
pedronunes19 <pedromiguelmnunes@gmail.com>
2022-12-21 21:51:35 +0000

    documentation

--- include/app.h
+++ include/app.h
@@ -22,6 +22,10 @@
 #define LOGGED_IN 230
 #define READY_PASSWORD 331
 
+/**
+ * @brief Parameters used for connection
+ * 
+ */
 typedef struct {
     char * user;
     char * password;
@@ -29,16 +33,61 @@ typedef struct {
     char * url_path;
 } connection_parameters;
 
+/**
+ * @brief Parse information from URL (RFC1738)
+ * 
+ * @param url URL to parse
+ * @return int 
+ */
 int parse_url(char * url);
 
+/**
+ * @brief Initialize connection_parameters struct according to parsed arguments 
+ * 
+ * @param user Username for login (NULL if anonymous connection)
+ * @param password Password for login (NULL if anonymous connection)
+ * @param host Hostname
+ * @param url_path Path to the file to be downloaded
+ * @return int 
+ */
 int initialize_connection_parameters(const char * user, const char * password, const char * host, const char * url_path);
 
-int open_connection(char *adress, int port);
+/**
+ * @brief Establish a TCP connection
+ * 
+ * @param address Connection address
+ * @param port Connection port
+ * @return int 
+ */
+int open_connection(char *address, int port);
 
+/**
+ * @brief Get the response code of a server response
+ * 
+ * @param file File descriptor to read from commands from the server
+ * @return int 
+ */
 int get_response_code(FILE* file);
 
+/**
+ * @brief Login to the server (can be anonymous)
+ * 
+ * @param fd File descriptor to read/send commands from/to the server
+ * @return int 
+ */
 int login(int fd);
 
+/**
+ * @brief Ask the server retrive the file and transfer it
+ * 
+ * @param fd File descriptor to read data/commands from the server
+ * @return int 
+ */
 int download(int fd);
 
+/**
+ * @brief FTP implementation, calls the right functions to download a file
+ * 
+ * @return int 
+ */
 int app();
